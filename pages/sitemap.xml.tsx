b17e661bc7361fcdbdbe2a8dc9249b7e2b3e9aa0
Kabir Chawla <kabir.438.2025@doonschool.com>
2022-12-16 11:12:17 +0530

    sitemap

---
+++ pages/sitemap.xml.tsx
@@ -0,0 +1,37 @@
+import { GetServerSideProps } from "next"
+
+function generateSiteMap() {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <url>
+       <loc>https://axioscareeracademy.com</loc>
+     </url>
+     <url>
+       <loc>https://axioscareeracademy.com/about-us</loc>
+     </url>
+     <url>
+       <loc>https://axioscareeracademy.com/contact-us</loc>
+     </url>
+     <url>
+       <loc>https://axioscareeracademy.com/courses</loc>
+     </url>
+     <url>
+       <loc>https://axioscareeracademy.com/gallery</loc>
+     </url>
+   </urlset>
+ `;
+}
+
+export default function SiteMap() {
+  return 
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  const sitemap = generateSiteMap();
+  res.setHeader('Content-Type', 'text/xml');
+  res.write(sitemap);
+  res.end();
+  return {
+    props: {}
+  }
+}
